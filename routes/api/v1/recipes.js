57e0e7d80de71a2b0d44058624ef7f1525fb702f
Matt Weiss <weiss.matt@outlook.com>
2019-07-06 13:05:16 -0600

    Adds ability to sort by any column name and either ascending or descending.
    Co-authored by: Jalena Taylor 
    <45905026+jalena-penaligon@users.noreply.github.com>

--- routes/api/v1/recipes.js
+++ routes/api/v1/recipes.js
@@ -3,27 +3,27 @@ var router = express.Router();
 var knex = require('../../../db/knex.js');
 
 router.get("/", function (req, res, next) {
-  knex('recipes').select()
-    .then((recipes) => {
-      res.status(200).json(recipes);
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
+  knex('recipes').orderBy(req.query.columnName, req.query.order)
+  .then((recipes) => {
+    res.status(200).json(recipes);
+  })
+  .catch((error) => {
+    res.status(500).json({ error });
+  });
 })
 
 router.get("/food_search", function (req, res, next) {
   knex('recipes').where('foodType', req.query.foodType)
-    .then((recipes) => {
-      if (recipes.length > 0) {
-        res.status(200).json(recipes);
-      } else {
-        res.status(404).json({error: "No recipes with that food type!"})
-      }
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
+  .then((recipes) => {
+    if (recipes.length > 0) {
+      res.status(200).json(recipes);
+    } else {
+      res.status(404).json({error: "No recipes with that food type!"})
+    }
+  })
+  .catch((error) => {
+    res.status(500).json({ error });
+  });
 })
 
 module.exports = router;
