f9210860e50f3c1dfb9296b5d1e53a54aec0810b
Matt Weiss <weiss.matt@outlook.com>
2019-07-06 14:01:19 -0600

    Adds column name validation
    Co-authored by: Jalena Taylor 
    <45905026+jalena-penaligon@users.noreply.github.com>

--- routes/api/v1/recipes.js
+++ routes/api/v1/recipes.js
@@ -3,28 +3,31 @@ var router = express.Router();
 var knex = require('../../../db/knex.js');
 
 router.get("/", function (req, res, next) {
-  if (req.query.order){
-    var sortOrder = req.query.order
+  if (req.query.order == 'desc'){
+    var sortOrder = 'desc'
   } else {
     var sortOrder = 'asc'
   }
-  if (req.query.columnName){
-    knex('recipes').orderBy(req.query.columnName, sortOrder)
-    .then((recipes) => {
-      res.status(200).json(recipes);
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
-  } else {
-    knex('recipes').select()
-    .then((recipes) => {
-      res.status(200).json(recipes);
-    })
-    .catch((error) => {
-      res.status(500).json({ error });
-    });
-  }
+  knex.schema.hasColumn('recipes', req.query.columnName)
+    .then(validity => {
+    if (validity == true){
+      knex('recipes').orderBy(req.query.columnName, sortOrder)
+      .then((recipes) => {
+        res.status(200).json(recipes);
+      })
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
+    } else {
+      knex('recipes').select()
+      .then((recipes) => {
+        res.status(200).json(recipes);
+      })
+      .catch((error) => {
+        res.status(500).json({ error });
+      });
+    }
+  })
 })
 
 router.get("/food_search", function (req, res, next) {
