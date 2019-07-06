c90570df9954e163fc283bff35be2f7e6b81378d
Matt Weiss <weiss.matt@outlook.com>
2019-07-06 12:41:28 -0600

    Adds error message for foodType with no entries
    Co-authored by: Jalena Taylor 
    <45905026+jalena-penaligon@users.noreply.github.com>

--- routes/api/v1/recipes.js
+++ routes/api/v1/recipes.js
@@ -15,7 +15,11 @@ router.get("/", function (req, res, next) {
 router.get("/food_search", function (req, res, next) {
   knex('recipes').where('foodType', req.query.foodType)
     .then((recipes) => {
-      res.status(200).json(recipes);
+      if (recipes.length > 0) {
+        res.status(200).json(recipes);
+      } else {
+        res.status(404).json({error: "No recipes with that food type!"})
+      }
     })
     .catch((error) => {
       res.status(500).json({ error });
