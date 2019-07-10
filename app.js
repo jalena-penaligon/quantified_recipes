f721ef7d8ebf8474546101ef4b8ec7aedce25781
Jalena Taylor <45905026+jalena-penaligon@users.noreply.github.com>
2019-07-10 15:04:10 -0600

    Adds CORS

--- app.js
+++ app.js
@@ -3,11 +3,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var recipesRouter = require('./routes/api/v1/recipes');
 
 var app = express();
+
+app.use(cors());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+  if ('OPTIONS' == req.method) {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 app.use(logger('dev'));
 app.use(express.json());
